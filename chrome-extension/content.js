4548b2fe4f8d986c93d41aa71980f505274b1aaf
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-07-23 17:17:14 -0400

    Init commit with working chrome extension

---
+++ chrome-extension/content.js
@@ -0,0 +1,37 @@
+const leaderboard = [
+  ...document.querySelectorAll("#currentLeaderboard table tbody td a")
+].map(a => a.href.split("/").pop());
+
+if (leaderboard.length > 0) {
+  appendDownloadLink(leaderboard, {
+    element: document.querySelector("#currentLeaderboard h1")
+  });
+}
+
+const myList = [...document.querySelectorAll("#yourList ol li a")].map(a =>
+  a.href.split("/").pop()
+);
+
+if (myList.length > 0) {
+  appendDownloadLink(myList, {
+    element: document.querySelector("#yourList h1"),
+    filename: "mylist.csv"
+  });
+}
+
+function appendDownloadLink(
+  ids,
+  {
+    text = "\u21E9 SAVE LETTERBOXD CSV",
+    filename = "leaderboard.csv",
+    element = document.body
+  }
+) {
+  const link = document.createElement("a");
+  link.style =
+    "display: block; padding: 3px 10px; font-size: 50%; position: relative; top: -3px;";
+  link.textContent = text;
+  link.download = filename;
+  link.href = "data:text/csv,tmdbID\n" + ids.join("\n");
+  element.appendChild(link);
+}
